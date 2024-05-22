d453248951a4682ec9ab64433861942b62397d0f
Susmoy Das <methundas269@gmail.com>
2024-05-22 17:15:15 +0600

    route ready

--- src/modules/order/order.route.ts
+++ src/modules/order/order.route.ts
@@ -1,13 +1,9 @@
 import express from "express";
-// import { ProductControllers } from "./product.controller";
+import { OrderControllers } from "./order.controller";
 
 const router = express.Router();
 
-// router.post("/", ProductControllers.createProduct);
-// router.get("/", ProductControllers.getAllProducts);
-// router.get("/:productId", ProductControllers.getSingleProduct);
-// router.get("/api/products", ProductControllers.searchProducts);
-// router.delete("/:productId", ProductControllers.deleteProduct);
-// router.put("/:productId", ProductControllers.updateProduct);
+router.post("/", OrderControllers.createOrder);
+router.get("/", OrderControllers.getAllOrder);
 
 export const OrderRoute = router;
