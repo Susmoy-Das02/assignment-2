25a0cf4a53f7e7bd078ad80fd879bcff8bc1952a
Susmoy Das <methundas269@gmail.com>
2024-05-22 17:28:27 +0600

    service ready for Orders by User Email

--- src/modules/order/order.servive.ts
+++ src/modules/order/order.servive.ts
@@ -9,7 +9,19 @@ const getAllOrder = async () => {
   const result = await Order.find();
   return result;
 };
+
+const getOrdersByEmail = async (email: string) => {
+  try {
+    const orders = await Order.find({ email });
+    return orders;
+  } catch (error) {
+    console.error("Error retrieving orders by email:", error);
+    throw new Error("Error retrieving orders by email: " + error);
+  }
+};
+
 export const OrderService = {
   createOrder,
   getAllOrder,
+  getOrdersByEmail,
 };
